e88301a273557304197ad95b894425289e857047
trucy <trucy.cc@gmail.com>
2023-06-23 16:36:05 +0800

    update

--- web/next.config.js
+++ web/next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    // 启用增量编译
-    experimental: {
-        optimizeCss: true, // CSS
-        workerThreads: true, // 启用 Worker Threads
-        pageEnv: true, // 启用页面环境
-        cpus: 1
-    },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
