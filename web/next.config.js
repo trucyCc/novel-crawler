bd22a0754766fd52b4f67a3a5ee5fb591a72264b
trucy <trucy.cc@gmail.com>
2023-06-23 16:26:18 +0800

    update

--- web/next.config.js
+++ web/next.config.js
@@ -3,10 +3,8 @@ const nextConfig = {
     // 启用增量编译
     experimental: {
         optimizeCss: true, // CSS
-        optimizeImages: true, // 图片
         workerThreads: true, // 启用 Worker Threads
         pageEnv: true, // 启用页面环境
-        concurrentFeatures: true, // 启用并行编译
     },
 }
 
